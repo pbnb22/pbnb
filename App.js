3795350a539a358878397db75feb30a8733a6a14
jm4189 <jm4189.dev@gmail.com>
2022-12-10 21:15:55 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+* Catch The Bug App
+* By JmPark
+**/
+import React, { useEffect, useState } from 'react';
+import { View,  Text} from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>
+        Init MainScreen
+      </Text>
+    </View>
+    
+  );
+};
+
+export default App;
