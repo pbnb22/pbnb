2acf431ffdf2cf6bd2e1cf31e8bfde7b2f0c24c7
jm4189 <jm4189.dev@gmail.com>
2022-12-11 12:07:54 +0900

    Change AppName & Icon

--- android/app/src/main/java/com/pbnb/MainActivity.java
+++ android/app/src/main/java/com/pbnb/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "pbnb";
+    return "빠밥늦밥";
   }
 
   /**
--- ios/빠밥늦밥Tests/빠밥늦밥Tests.m
+++ ios/빠밥늦밥Tests/빠밥늦밥Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface pbnbTests : XCTestCase
+@interface 빠밥늦밥Tests : XCTestCase
 
 @end
 
-@implementation pbnbTests
+@implementation 빠밥늦밥Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
